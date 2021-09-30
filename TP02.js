2fdf505662578afbfd29d5041c61e31161a24acb
Samsamb0 <91201315+Samsamb0@users.noreply.github.com>
2021-09-30 15:33:59 +0200

    Update TP02.js

--- TP02.js
+++ TP02.js
@@ -1,10 +1,20 @@
-let motDePasse;
+/*let motDePasse;
 motDePasse = prompt("veuillez introduire le mot de passe");
 	if (motDePasse == "Césame"){
 	console.log("Ouvert");
 	}
 		else{
-		console.log("t'es con");
+		console.log("sorry");
 		}
-		
-    
+		/*
+let nombreMessages;
+nombreMessages = prompt("Veuillez afficher votre nombres de messges");
+	if (nombreMessages>1) {
+		console.log("Vous avez x nouveau messages.");
+	} else if ( nombreMessages == 1) {
+		console.log("Vous avez 1 nouveau message");
+	} else{
+		console.log("Vous n'avez pas d enouveau message");
+	}
+	
+			
