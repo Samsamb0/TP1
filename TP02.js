1d1a464d7318cfd70287d5c09bdf5c7b24fc4227
Samsamb0 <91201315+Samsamb0@users.noreply.github.com>
2021-09-30 15:03:53 +0200

    Create TP02.js

---
+++ TP02.js
@@ -0,0 +1,10 @@
+let motDePasse;
+motDePasse = prompt("veuillez introduire le mot de passe");
+	if (motDePasse == "Césame"){
+	console.log("Ouvert");
+	}
+		else{
+		console.log("t'es con");
+		}
+		
+    
